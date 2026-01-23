e7cbbfe0abdf9cdb813be25ca70962aea39ddc37
xliu343 <xliu343@ucsc.edu>
2026-01-22 21:42:09 -0800

    adding Journal 1 session 2 prompt

---
+++ cjs2.py
@@ -0,0 +1,21 @@
+def printsumflfl():
+	a = 3.5
+	b = 2.2
+	sum_result = a + b
+	print("Sum:", sum_result, "Type:", type(sum_result))
+def printdifferenceintint():
+	x = 10
+	y = 4
+	diff_result = x - y
+	print("Difference:", diff_result, "Type:", type(diff_result))
+def printproductflint():
+	m = 2.5
+	n = 6
+	product_result = m * n
+	print("Product:", product_result, "Type:", type(product_result))
+def main():
+	printsumflfl()
+	printdifferenceintint()
+	printproductflint()
+if __name__ == '__main__':
+	main()

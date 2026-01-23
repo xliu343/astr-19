74b3ead64f45bf1dad480ca99557729fb4392b1f
xliu343 <xliu343@ucsc.edu>
2026-01-22 21:21:24 -0800

    Add files via upload

---
+++ cjs1.py
@@ -0,0 +1,10 @@
+def printname():
+	print("I am Xingyu Liu")
+def printmandf():
+	print("I like StarWar movies and pizza")
+def main():
+	printname()
+	printmandf()
+if __name__=="__main__":
+	main()
+
